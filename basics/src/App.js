d9c4296a39f5454822f60694afb1e650d0ab25fd
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2022-11-26 00:23:07 +0000

    Header  Content  and Total  component was created

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
 
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}</p>
     </div>
   )
 }
 
 const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+    
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    
     </div>
   )
 }
