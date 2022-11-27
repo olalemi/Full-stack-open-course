5c456ba35440d6b51fa2dbe5d37a8360b24a14fe
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2022-11-27 23:29:11 +0000

    part 1 of 1

---
+++ Part 1/JavaScript.js
@@ -0,0 +1,12 @@
+// Variables
+// There are a few ways to define variables in JavaScript
+
+const x = 1
+let y = 5
+
+console.log(x, y)   // tulostuu 1, 5
+
+console.log(x, y)   // tulostuu 1, 15
+
+console.log(x, y)   // tulostuu 1, teksti
+           
