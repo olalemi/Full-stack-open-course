a86d678a3fb84a0696683b545fba514195922024
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2022-12-01 23:20:42 +0000

    header , content and total component added

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
-}
+
+
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maya" />
-      <Hello name="Nurudeen" />
-      <Hello name="Peka" />
+     
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+
     </div>
   )
 }
